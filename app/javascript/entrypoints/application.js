cde80dec12db85f79712c8f3858b8b57cb211a4a
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-09 12:34:48 +0900

    add: Vueファイルの作成

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -26,3 +26,9 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+import { createApp } from 'vue'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  createApp(App).mount('#app')
+})
